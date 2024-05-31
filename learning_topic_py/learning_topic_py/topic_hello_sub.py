258055c2bc731cb755d27cf44bc4888c912b31da
boran <boranyang3@gmail.com>
2024-05-31 11:27:57 +0800

    topic

--- learning_topic_py/learning_topic_py/topic_hello_sub.py
+++ learning_topic_py/learning_topic_py/topic_hello_sub.py
@@ -7,7 +7,7 @@ class HelloSubNode(Node):
 
     def __init__(self,name):
         super().__init__(name)
-        self.subscription = self.create_subscription(String, 'hello', self.listener_callback, 10)
+        self.subscription = self.create_subscription(String, 'chat', self.listener_callback, 10)
     
     def listener_callback(self,msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
